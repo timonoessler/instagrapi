bfb15f41895561e088eb15ab77561d75f5616b76
timonoessler <timo.nowag@icloud.com>
2024-12-04 14:42:54 +0100

    Added main.py with minimum amount to get Flask running

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import helper.api as api
+
+def main():
+    app = api.FlaskAPIServer().get_app()
+    return app
+
+if __name__ == '__main__':
+    main_app = main()
+    main_app.run(host='127.0.0.1', port=5000, debug=True)
